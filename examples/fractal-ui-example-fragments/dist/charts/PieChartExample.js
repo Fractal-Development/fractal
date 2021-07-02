8c6b688bbf0d8d3e5921d87755c549d77486431a
brandonmaldonadoalonso <brandonma98@protonmail.com>
2021-07-01 19:06:21 -0500

    Removed react native direct import

--- examples/fractal-ui-example-fragments/dist/charts/PieChartExample.js
+++ examples/fractal-ui-example-fragments/dist/charts/PieChartExample.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
-import { Button } from 'react-native';
-import { Box, HorizontalLayer, useTheme } from '@bma98/fractal-ui';
+import { Box, HorizontalLayer, useTheme, Button } from '@bma98/fractal-ui';
 import { PieChart } from '@bma98/fractal-charts';
 export function PieChartExample({ data }) {
     const [selectedIndex, setSelectedIndex] = useState(undefined);
@@ -11,8 +10,8 @@ export function PieChartExample({ data }) {
     return (React.createElement(Box, { marginBottom: spacings.m },
         React.createElement(PieChart, { height: 200, data: data, selectedSliceIndex: selectedIndex }),
         React.createElement(HorizontalLayer, { justifyContent: 'space-evenly', marginTop: 20 },
-            React.createElement(Button, { title: 'One', color: data[0].color, onPress: () => handleIndex(0) }),
-            React.createElement(Button, { title: 'Two', color: data[1].color, onPress: () => handleIndex(1) }),
-            React.createElement(Button, { title: 'Three', color: data[2].color, onPress: () => handleIndex(2) }))));
+            React.createElement(Button, { text: 'One', backgroundColor: data[0].color, onPress: () => handleIndex(0) }),
+            React.createElement(Button, { text: 'Two', backgroundColor: data[1].color, onPress: () => handleIndex(1) }),
+            React.createElement(Button, { text: 'Three', backgroundColor: data[2].color, onPress: () => handleIndex(2) }))));
 }
 //# sourceMappingURL=PieChartExample.js.map
--- examples/fractal-ui-example-fragments/src/charts/PieChartExample.tsx
+++ examples/fractal-ui-example-fragments/src/charts/PieChartExample.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
-import { Button } from 'react-native';
-import { Box, HorizontalLayer, useTheme } from '@bma98/fractal-ui';
+import { Box, HorizontalLayer, useTheme, Button } from '@bma98/fractal-ui';
 import { PieChart, PieChartData } from '@bma98/fractal-charts';
 
 interface Props {
@@ -19,9 +18,9 @@ export function PieChartExample({ data }: Props): JSX.Element {
         <Box marginBottom={spacings.m}>
             <PieChart height={200} data={data} selectedSliceIndex={selectedIndex} />
             <HorizontalLayer justifyContent={'space-evenly'} marginTop={20}>
-                <Button title={'One'} color={data[0].color} onPress={() => handleIndex(0)} />
-                <Button title={'Two'} color={data[1].color} onPress={() => handleIndex(1)} />
-                <Button title={'Three'} color={data[2].color} onPress={() => handleIndex(2)} />
+                <Button text={'One'} backgroundColor={data[0].color} onPress={() => handleIndex(0)} />
+                <Button text={'Two'} backgroundColor={data[1].color} onPress={() => handleIndex(1)} />
+                <Button text={'Three'} backgroundColor={data[2].color} onPress={() => handleIndex(2)} />
             </HorizontalLayer>
         </Box>
     );
