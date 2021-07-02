f1f6014d1989fbf23619184c4bbefbe2e97db675
baltazar-gr <69155714+baltazar-gr@users.noreply.github.com>
2021-07-02 14:36:32 -0500

    change dependencies of fractal-ui-expo-example for use GitPkg

--- examples/fractal-ui-expo-example/metro.config.js
+++ examples/fractal-ui-expo-example/metro.config.js
@@ -1,45 +1,5 @@
-/**
- * Metro configuration for React Native
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-const path = require('path');
-
-// Listen for our libraries in their respective folder inside the monorepo
-const fractalUI = path.resolve(__dirname + '/../../packages/fractal-ui');
-const fractalCharts = path.resolve(__dirname + '/../../packages/fractal-charts');
-const fractalUIEXampleFragmentsPath = path.resolve(__dirname + '/../fractal-ui-example-fragments');
-
 module.exports = {
     transformer: {
-        getTransformOptions: async () => ({
-            transform: {
-                experimentalImportSupport: false,
-                inlineRequires: true
-            }
-        })
-    },
-    resolver: {
-        extraNodeModules: new Proxy(
-            {
-                '@bma98/fractal-ui': fractalUI,
-                '@bma98/fractal-charts': fractalCharts,
-                'fractal-ui-example-fragments': fractalUIEXampleFragmentsPath
-            },
-            {
-                // This will resolve dependencies on the previous list
-                // Is similar to webpack's config.resolve.alias['react']
-                get: (target, name) => {
-                    if (target.hasOwnProperty(name)) {
-                        return target[name];
-                    }
-
-                    return path.join(__dirname, `/node_modules/${name}`);
-                }
-            }
-        )
-    },
-    watchFolders: [fractalUIEXampleFragmentsPath, fractalUI, fractalCharts]
+        assetPlugins: ['expo-asset/tools/hashAssetFiles']
+    }
 };
