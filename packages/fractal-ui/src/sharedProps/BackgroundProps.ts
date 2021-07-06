6b484d453acb4b53e2427d8d452d2eb1039cc001
brandonmaldonadoalonso <brandonma98@protonmail.com>
2021-07-06 16:54:18 -0500

    fixes

--- packages/fractal-ui/src/sharedProps/BackgroundProps.ts
+++ packages/fractal-ui/src/sharedProps/BackgroundProps.ts
@@ -1,11 +1,9 @@
 export interface BackgroundProps {
     backgroundColor?: string;
-    bg?: string;
 }
 
-export function extractBackgroundProps({ backgroundColor, bg }: BackgroundProps): string {
+export function extractBackgroundProps({ backgroundColor }: BackgroundProps): string {
     return `
-        background-color: ${bg ?? 'transparent'};
         background-color: ${backgroundColor ?? 'transparent'};
     `;
 }
