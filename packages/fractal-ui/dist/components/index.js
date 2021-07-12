82680852982168f3bbd405cfd957598a6f1ab412
brandonmaldonadoalonso <brandonma98@protonmail.com>
2021-07-12 00:06:50 -0500

    bug fixes

--- packages/fractal-ui/dist/components/index.js
+++ packages/fractal-ui/dist/components/index.js
@@ -1,11 +1,11 @@
 export * from './animation';
+export * from './layout';
+export * from './text';
+export * from './overlays';
 export * from './feedback';
 export * from './dataDisplay';
-export * from './overlays';
 export * from './interactive';
-export * from './layout';
 export * from './media';
 export * from './platform';
-export * from './text';
 export * from './ThemeContent';
 //# sourceMappingURL=index.js.map
--- packages/fractal-ui/dist/components/interactive/buttons/index.js
+++ packages/fractal-ui/dist/components/interactive/buttons/index.js
@@ -3,13 +3,13 @@ export * from './Pressable';
 export * from './TouchableOpacity';
 export * from './TouchableOpacity/types';
 export * from './Button';
+export * from './socialButtons';
 export * from './TextButton';
 export * from './ToggleButton';
 export * from './RoundedToggleButton';
 export * from './CrossButton';
 export * from './CircularIconButton';
 export * from './OptionsButton';
-export * from './types';
 export * from './OptionsMenuButton';
-export * from './socialButtons';
+export * from './types';
 //# sourceMappingURL=index.js.map
