facbdb7be009f77aac0016373aa4c31ae4aa608b
RVFractal <rubenvillegas@fractal.software>
2021-07-13 15:57:03 -0500

    fractal-examples: Improved ChipList example

--- packages/fractal-examples/dist/ui-fragments/dataDisplay/ChipListFragment.js
+++ packages/fractal-examples/dist/ui-fragments/dataDisplay/ChipListFragment.js
@@ -3,12 +3,19 @@ import { useTheme, ChipList, Layer, Text } from '@bma98/fractal-ui';
 import { getTitleTextAccessibilityProps } from '../accessibility/getTitleTextAccessibilityProps';
 export function ChipListFragment() {
     const { spacings } = useTheme();
+    const tags = [
+        { id: '0', value: 'tag 1' },
+        { id: '1', value: 'tag 2' },
+        { id: '2', value: 'tag 3' },
+        { id: '3', value: 'tag 4' },
+        { id: '3', value: 'tag 5' }
+    ];
     return (React.createElement(React.Fragment, null,
         React.createElement(Text, Object.assign({ variant: 'title' }, getTitleTextAccessibilityProps(1)), "Chip List Fragment"),
         React.createElement(Layer, { flexDirection: 'row', marginTop: spacings.s, marginBottom: spacings.xl },
-            React.createElement(ChipList, { data: [], getLabel: (item) => {
+            React.createElement(ChipList, { data: tags, getLabel: (item) => {
                     console.log(item);
-                    return item;
+                    return item.value;
                 }, onItemPress: (item) => {
                     console.log(item);
                 } }))));
--- packages/fractal-examples/src/ui-fragments/dataDisplay/ChipListFragment.tsx
+++ packages/fractal-examples/src/ui-fragments/dataDisplay/ChipListFragment.tsx
@@ -2,8 +2,21 @@ import React from 'react';
 import { useTheme, ChipList, Layer, Text } from '@bma98/fractal-ui';
 import { getTitleTextAccessibilityProps } from '../accessibility/getTitleTextAccessibilityProps';
 
+interface Tag {
+    id: string;
+    value: string;
+}
+
 export function ChipListFragment(): JSX.Element {
     const { spacings } = useTheme();
+
+    const tags: Array<Tag> = [
+        { id: '0', value: 'tag 1' },
+        { id: '1', value: 'tag 2' },
+        { id: '2', value: 'tag 3' },
+        { id: '3', value: 'tag 4' },
+        { id: '3', value: 'tag 5' }
+    ];
 
     return (
         <>
@@ -12,10 +25,10 @@ export function ChipListFragment(): JSX.Element {
             </Text>
             <Layer flexDirection={'row'} marginTop={spacings.s} marginBottom={spacings.xl}>
                 <ChipList
-                    data={[]}
+                    data={tags}
                     getLabel={(item) => {
                         console.log(item);
-                        return item;
+                        return item.value;
                     }}
                     onItemPress={(item) => {
                         console.log(item);
