d5523d95b109d3f6d244a11883b6fe42bebc210b
RVFractal <rubenvillegas@fractal.software>
2021-07-13 16:25:42 -0500

    fractal-examples: Added Dimmed Modal example

---
+++ packages/fractal-examples/dist/ui-fragments/overlays/modals/DimmedModalFragment.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import { useTheme, Box, Button, Text, DimmedModal } from '@bma98/fractal-ui';
+import { getTitleTextAccessibilityProps } from '../../accessibility/getTitleTextAccessibilityProps';
+export function MiddleCellModalFragment() {
+    const { spacings } = useTheme();
+    const [dimmedModalVisible, setDimmedModalVisible] = useState(false);
+    const toggleDimmedModal = () => setDimmedModalVisible((currentValue) => !currentValue);
+    return (React.createElement(React.Fragment, null,
+        React.createElement(Text, Object.assign({ variant: 'title' }, getTitleTextAccessibilityProps(1)), "Dimmed Modal Fragment"),
+        React.createElement(Box, { marginTop: spacings.s, marginBottom: spacings.xl },
+            React.createElement(Button, { variant: 'main', text: 'Show Dimmed Modal', onPress: toggleDimmedModal }),
+            React.createElement(DimmedModal, { visible: dimmedModalVisible, onDismiss: toggleDimmedModal }))));
+}
+//# sourceMappingURL=DimmedModalFragment.js.map
---
+++ packages/fractal-examples/src/ui-fragments/overlays/modals/DimmedModalFragment.tsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import { useTheme, Box, Button, Text, DimmedModal } from '@bma98/fractal-ui';
+import { getTitleTextAccessibilityProps } from '../../accessibility/getTitleTextAccessibilityProps';
+
+export function MiddleCellModalFragment(): JSX.Element {
+    const { spacings } = useTheme();
+    const [dimmedModalVisible, setDimmedModalVisible] = useState(false);
+
+    const toggleDimmedModal = () => setDimmedModalVisible((currentValue) => !currentValue);
+
+    return (
+        <>
+            <Text variant={'title'} {...getTitleTextAccessibilityProps(1)}>
+                Dimmed Modal Fragment
+            </Text>
+            <Box marginTop={spacings.s} marginBottom={spacings.xl}>
+                <Button variant='main' text='Show Dimmed Modal' onPress={toggleDimmedModal} />
+                <DimmedModal visible={dimmedModalVisible} onDismiss={toggleDimmedModal} />
+            </Box>
+        </>
+    );
+}
