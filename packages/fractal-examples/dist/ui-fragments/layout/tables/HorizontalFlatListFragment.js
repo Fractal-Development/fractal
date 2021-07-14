f794fe50cbbe78ac51d577a440608b3c7c5c048c
RVFractal <rubenvillegas@fractal.software>
2021-07-14 16:57:48 -0500

    fractal-examples: Updated HorizontalFlatList

--- packages/fractal-examples/dist/ui-fragments/layout/tables/HorizontalFlatListFragment.js
+++ packages/fractal-examples/dist/ui-fragments/layout/tables/HorizontalFlatListFragment.js
@@ -1,19 +1,17 @@
 import React, { useEffect, useState } from 'react';
-import { Layer, HorizontalFlatList, SearchBar, TableContainer, Text, useTheme } from '@bma98/fractal-ui';
+import { Layer, HorizontalFlatList, Text, useTheme } from '@bma98/fractal-ui';
 import { useSizeValue } from '@bma98/size-class';
 import { getTitleTextAccessibilityProps } from '../../accessibility/getTitleTextAccessibilityProps';
 import { dataProvider, rowRendererHorizontalTable, tableDummyData } from './tableHelpers';
 function Table() {
-    const { spacings, sizes } = useTheme();
+    const { sizes } = useTheme();
     const [dataProviderState, setDataProviderState] = useState(dataProvider.cloneWithRows(tableDummyData));
     const width = useSizeValue('width');
     useEffect(() => {
         setDataProviderState(dataProvider.cloneWithRows(tableDummyData));
     }, [width]);
-    return (React.createElement(TableContainer, { title: 'Horizontal FlatList', flex: 1 },
-        React.createElement(SearchBar, { placeholder: 'Buscar', buttonText: 'Buscar', marginBottom: spacings.lg, enableSearchButton: true }),
-        React.createElement(Layer, { flex: 1 },
-            React.createElement(HorizontalFlatList, { key: width, rowWidth: 100, rowHeight: sizes.baseRowHeight, dataProvider: dataProviderState, rowRenderer: rowRendererHorizontalTable }))));
+    return (React.createElement(Layer, { flex: 1 },
+        React.createElement(HorizontalFlatList, { key: width, rowWidth: 100, rowHeight: sizes.baseRowHeight, dataProvider: dataProviderState, rowRenderer: rowRendererHorizontalTable })));
 }
 export function HorizontalFlatListFragment() {
     const { spacings } = useTheme();
--- packages/fractal-examples/src/ui-fragments/layout/tables/HorizontalFlatListFragment.tsx
+++ packages/fractal-examples/src/ui-fragments/layout/tables/HorizontalFlatListFragment.tsx
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from 'react';
-import { Layer, HorizontalFlatList, SearchBar, TableContainer, Text, useTheme } from '@bma98/fractal-ui';
+import { Layer, HorizontalFlatList, Text, useTheme } from '@bma98/fractal-ui';
 import { useSizeValue } from '@bma98/size-class';
 import { getTitleTextAccessibilityProps } from '../../accessibility/getTitleTextAccessibilityProps';
 import { dataProvider, rowRendererHorizontalTable, tableDummyData } from './tableHelpers';
 
 function Table(): JSX.Element {
-    const { spacings, sizes } = useTheme();
+    const { sizes } = useTheme();
     const [dataProviderState, setDataProviderState] = useState(dataProvider.cloneWithRows(tableDummyData));
     const width = useSizeValue('width');
 
@@ -14,18 +14,15 @@ function Table(): JSX.Element {
     }, [width]);
 
     return (
-        <TableContainer title={'Horizontal FlatList'} flex={1}>
-            <SearchBar placeholder={'Buscar'} buttonText={'Buscar'} marginBottom={spacings.lg} enableSearchButton />
-            <Layer flex={1}>
-                <HorizontalFlatList
-                    key={width}
-                    rowWidth={100}
-                    rowHeight={sizes.baseRowHeight}
-                    dataProvider={dataProviderState}
-                    rowRenderer={rowRendererHorizontalTable}
-                />
-            </Layer>
-        </TableContainer>
+        <Layer flex={1}>
+            <HorizontalFlatList
+                key={width}
+                rowWidth={100}
+                rowHeight={sizes.baseRowHeight}
+                dataProvider={dataProviderState}
+                rowRenderer={rowRendererHorizontalTable}
+            />
+        </Layer>
     );
 }
 
