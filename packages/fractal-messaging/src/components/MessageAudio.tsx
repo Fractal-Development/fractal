53577c606d02808a6e1f22d4178ff7d8966320bc
baltazar-gr <69155714+baltazar-gr@users.noreply.github.com>
2021-07-16 11:20:39 -0500

    fractal-messaging: update MessageAudio

--- packages/fractal-messaging/src/components/MessageAudio.tsx
+++ packages/fractal-messaging/src/components/MessageAudio.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import { AudioMessagePlayer } from '@bma98/fractal-media';
 import { MessageMediaProps } from './types';
 import { MESSAGE_AUDIO_HEIGHT } from '../constants';
+import { useChatMessageSize } from '../hooks/useChatMessageSize';
 
 export function MessageAudio({ source }: MessageMediaProps): JSX.Element {
-    return <AudioMessagePlayer audioSrc={source} height={MESSAGE_AUDIO_HEIGHT} />;
+    const { width } = useChatMessageSize();
+    return <AudioMessagePlayer audioSrc={source} width={width} height={MESSAGE_AUDIO_HEIGHT} />;
 }
