1dd66d07f133fc67e6cb91dbd36336ab1bc476c0
baltazar-gr <69155714+baltazar-gr@users.noreply.github.com>
2021-07-19 14:33:49 -0500

    fractal-ui-next-js-example: fixes

--- examples/fractal-ui-next-js-example/next.config.js
+++ examples/fractal-ui-next-js-example/next.config.js
@@ -1,4 +1,13 @@
-const withTM = require('next-transpile-modules')(['@bma98/fractal-charts', '@bma98/fractal-examples', '@bma98/fractal-ui', '@bma98/size-class']);
+const withTM = require('next-transpile-modules')([
+    '@bma98/fractal-charts',
+    '@bma98/fractal-examples',
+    '@bma98/fractal-ui',
+    '@bma98/size-class',
+    '@bma98/fractal-media',
+    '@bma98/fractal-messaging',
+    'd3-shape',
+    'd3-path'
+]);
 
 module.exports = withTM({
     reactStrictMode: true,
