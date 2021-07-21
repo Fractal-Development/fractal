cf28067c0bcc9d04ad8f160fe8ff0b607cc9821c
RVFractal <rubenvillegas@fractal.software>
2021-07-21 13:00:18 -0500

    fractal-examples: Improved IconsFragment

--- packages/fractal-examples/dist/ui-fragments/IconsFragment.js
+++ packages/fractal-examples/dist/ui-fragments/IconsFragment.js
@@ -5,17 +5,17 @@ export function IconsFragment() {
     const { spacings } = useTheme();
     return (React.createElement(React.Fragment, null,
         React.createElement(Text, Object.assign({ variant: 'title' }, getTitleTextAccessibilityProps(1)), "Icons Fragment"),
-        React.createElement(Box, { marginTop: spacings.s, marginBottom: spacings.xl },
-            React.createElement(AppleIcon, null),
-            React.createElement(BugIcon, null),
-            React.createElement(CheckIcon, null),
-            React.createElement(ChevronDownIcon, null),
-            React.createElement(CloseIcon, null),
-            React.createElement(FacebookIcon, null),
-            React.createElement(FileIcon, null),
-            React.createElement(GoogleIcon, null),
-            React.createElement(LoadIcon, null),
-            React.createElement(SearchIcon, null),
-            React.createElement(ThreeDotsHorizontalIcon, null))));
+        React.createElement(Box, { flexDirection: 'row', marginTop: spacings.s, marginBottom: spacings.xl },
+            React.createElement(AppleIcon, { height: 24, width: 24 }),
+            React.createElement(BugIcon, { height: 24, width: 24 }),
+            React.createElement(CheckIcon, { height: 24, width: 24 }),
+            React.createElement(ChevronDownIcon, { height: 24, width: 24 }),
+            React.createElement(CloseIcon, { height: 24, width: 24 }),
+            React.createElement(FacebookIcon, { height: 24, width: 24 }),
+            React.createElement(FileIcon, { height: 24, width: 24 }),
+            React.createElement(GoogleIcon, { height: 24, width: 24 }),
+            React.createElement(LoadIcon, { height: 24, width: 24 }),
+            React.createElement(SearchIcon, { height: 24, width: 24 }),
+            React.createElement(ThreeDotsHorizontalIcon, { height: 24, width: 24 }))));
 }
 //# sourceMappingURL=IconsFragment.js.map
--- packages/fractal-examples/src/ui-fragments/IconsFragment.tsx
+++ packages/fractal-examples/src/ui-fragments/IconsFragment.tsx
@@ -1,5 +1,20 @@
 import React from 'react';
-import { useTheme, Box, Text, AppleIcon, BugIcon, CheckIcon, ChevronDownIcon, CloseIcon, FacebookIcon, FileIcon, GoogleIcon, LoadIcon, SearchIcon, ThreeDotsHorizontalIcon } from '@bma98/fractal-ui';
+import {
+    useTheme,
+    Box,
+    Text,
+    AppleIcon,
+    BugIcon,
+    CheckIcon,
+    ChevronDownIcon,
+    CloseIcon,
+    FacebookIcon,
+    FileIcon,
+    GoogleIcon,
+    LoadIcon,
+    SearchIcon,
+    ThreeDotsHorizontalIcon
+} from '@bma98/fractal-ui';
 import { getTitleTextAccessibilityProps } from './accessibility/getTitleTextAccessibilityProps';
 
 export function IconsFragment(): JSX.Element {
@@ -10,18 +25,18 @@ export function IconsFragment(): JSX.Element {
             <Text variant={'title'} {...getTitleTextAccessibilityProps(1)}>
                 Icons Fragment
             </Text>
-            <Box marginTop={spacings.s} marginBottom={spacings.xl}>
-                <AppleIcon />
-                <BugIcon />
-                <CheckIcon />
-                <ChevronDownIcon />
-                <CloseIcon />
-                <FacebookIcon />
-                <FileIcon />
-                <GoogleIcon />
-                <LoadIcon />
-                <SearchIcon />
-                <ThreeDotsHorizontalIcon />
+            <Box flexDirection={'row'} marginTop={spacings.s} marginBottom={spacings.xl}>
+                <AppleIcon height={24} width={24} />
+                <BugIcon height={24} width={24} />
+                <CheckIcon height={24} width={24} />
+                <ChevronDownIcon height={24} width={24} />
+                <CloseIcon height={24} width={24} />
+                <FacebookIcon height={24} width={24} />
+                <FileIcon height={24} width={24} />
+                <GoogleIcon height={24} width={24} />
+                <LoadIcon height={24} width={24} />
+                <SearchIcon height={24} width={24} />
+                <ThreeDotsHorizontalIcon height={24} width={24} />
             </Box>
         </>
     );
