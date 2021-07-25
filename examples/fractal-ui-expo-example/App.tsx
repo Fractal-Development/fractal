b20fe92f58cabbb588aff8738b70e1aa9632735e
brandonmaldonadoalonso <brandonma98@protonmail.com>
2021-07-25 13:42:05 -0500

    added temporal navigation bar to test if it works

--- examples/fractal-ui-expo-example/App.tsx
+++ examples/fractal-ui-expo-example/App.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Screen, ScreenStack } from 'react-native-screens';
 import { MainExample } from '@bma98/fractal-examples';
-import { NativeNavigationBar } from '@bma98/fractal-navigation';
+import { NativeNavigationBar, NativeNavigationBarRightView, NavigationBarButton } from '@bma98/fractal-navigation';
 
 export default function App() {
     return (
-        <ScreenStack>
+        <ScreenStack style={{ flex: 1 }}>
             <Screen>
-                <NativeNavigationBar goBack={() => {}} />
+                <NativeNavigationBar title={'Examples'} goBack={() => {}}>
+                    <NativeNavigationBarRightView>
+                        <NavigationBarButton>Prueba</NavigationBarButton>
+                    </NativeNavigationBarRightView>
+                </NativeNavigationBar>
                 <MainExample />
             </Screen>
         </ScreenStack>
