cd9b1331279f643cb2dac255c21560366d1395b9
brandonmaldonadoalonso <brandonma98@protonmail.com>
2021-07-25 23:16:45 -0500

    added inital example implementation of fractal navigation

--- examples/fractal-ui-expo-example/App.tsx
+++ examples/fractal-ui-expo-example/App.tsx
@@ -1,19 +1,34 @@
 import React from 'react';
 import { Screen, ScreenStack } from 'react-native-screens';
 import { MainExample } from '@bma98/fractal-examples';
-import { NativeNavigationBar, NativeNavigationBarRightView, NavigationBarButton } from '@bma98/fractal-navigation';
+import {
+    NativeNavigationBar,
+    NativeNavigationBarRightView,
+    NavigationBarButton,
+    TabBar,
+    TabBarContextProvider,
+    NavigationLayer
+} from '@bma98/fractal-navigation';
+import { Layer } from '@bma98/fractal-ui';
 
 export default function App() {
     return (
-        <ScreenStack style={{ flex: 1 }}>
-            <Screen>
-                <NativeNavigationBar title={'Examples'} goBack={() => {}}>
-                    <NativeNavigationBarRightView>
-                        <NavigationBarButton>Prueba</NavigationBarButton>
-                    </NativeNavigationBarRightView>
-                </NativeNavigationBar>
-                <MainExample />
-            </Screen>
-        </ScreenStack>
+        <Layer height={'100vh'}>
+            <TabBarContextProvider>
+                <ScreenStack style={{ flex: 1 }}>
+                    <Screen>
+                        <NativeNavigationBar title={'Examples'} goBack={() => {}}>
+                            <NativeNavigationBarRightView>
+                                <NavigationBarButton>Prueba</NavigationBarButton>
+                            </NativeNavigationBarRightView>
+                        </NativeNavigationBar>
+                        <NavigationLayer>
+                            <MainExample />
+                        </NavigationLayer>
+                    </Screen>
+                </ScreenStack>
+                <TabBar tabBarPosition={'bottom'} />
+            </TabBarContextProvider>
+        </Layer>
     );
 }
