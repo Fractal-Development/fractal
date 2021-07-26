8708ece1def74c241a6d2511b9c2b198b0a09d8c
brandonmaldonadoalonso <brandonma98@protonmail.com>
2021-07-25 22:44:48 -0500

    added missing imports

--- packages/fractal-navigation/dist/TabBar/index.d.ts
+++ packages/fractal-navigation/dist/TabBar/index.d.ts
@@ -2,3 +2,5 @@ export * from './hooks';
 export * from './types';
 export * from './util';
 export * from './TabBarBackground';
+export * from './TabBar';
+export * from './TabBarContextProvider';
--- packages/fractal-navigation/dist/TabBar/index.js
+++ packages/fractal-navigation/dist/TabBar/index.js
@@ -2,4 +2,6 @@ export * from './hooks';
 export * from './types';
 export * from './util';
 export * from './TabBarBackground';
+export * from './TabBar';
+export * from './TabBarContextProvider';
 //# sourceMappingURL=index.js.map
