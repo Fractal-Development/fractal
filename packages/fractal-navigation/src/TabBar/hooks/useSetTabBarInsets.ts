3ec6bf7943d5d3675de4ad135c31cb17823c642d
brandonmaldonadoalonso <brandonma98@protonmail.com>
2021-08-04 14:44:47 -0500

    added safe area insets to tabbarinsets

--- packages/fractal-navigation/src/TabBar/hooks/useSetTabBarInsets.ts
+++ packages/fractal-navigation/src/TabBar/hooks/useSetTabBarInsets.ts
@@ -3,19 +3,21 @@ import { useTabBarIsHidden } from './useTabBarIsHidden';
 import { TabBarInsetsContext } from '../context/TabBarInsetsProvider';
 import { useTabBarPosition } from './useTabBarPosition';
 import { useTabBarSizeForPosition } from './useTabBarSizeForPosition';
+import { useSafeAreaInsets } from './useSafeAreaInsets';
 
 export function useSetTabBarInsets(): void {
     const tabBarPosition = useTabBarPosition();
     const tabBarIsHidden = useTabBarIsHidden();
     const size = useTabBarSizeForPosition(tabBarPosition, tabBarIsHidden);
+    const safeAreaInsets = useSafeAreaInsets();
     const [, setTabBarInsets] = useContext(TabBarInsetsContext);
 
     useLayoutEffect(() => {
         setTabBarInsets({
             top: 0,
-            right: tabBarPosition === 'right' ? size : 0,
-            bottom: tabBarPosition === 'bottom' ? size : 0,
-            left: tabBarPosition === 'left' ? size : 0
+            right: tabBarPosition === 'right' ? size + safeAreaInsets.right : 0,
+            bottom: tabBarPosition === 'bottom' ? size + safeAreaInsets.bottom : 0,
+            left: tabBarPosition === 'left' ? size + safeAreaInsets.left : 0
         });
-    }, [tabBarPosition, setTabBarInsets, size]);
+    }, [tabBarPosition, setTabBarInsets, size, safeAreaInsets]);
 }
