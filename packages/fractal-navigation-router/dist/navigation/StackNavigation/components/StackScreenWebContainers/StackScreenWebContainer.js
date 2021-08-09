3aa209dfcb6c056bcc1b64b952536d19bca9d742
brandonmaldonadoalonso <brandonma98@protonmail.com>
2021-08-08 22:36:41 -0500

    bug fixes

--- packages/fractal-navigation-router/dist/navigation/StackNavigation/components/StackScreenWebContainers/StackScreenWebContainer.js
+++ packages/fractal-navigation-router/dist/navigation/StackNavigation/components/StackScreenWebContainers/StackScreenWebContainer.js
@@ -1,5 +1,6 @@
 import React from 'react';
+import { Background } from '@bma98/fractal-ui';
 export function StackScreenWebContainer({ children }) {
-    return React.createElement(React.Fragment, null, children);
+    return (React.createElement(Background, { top: 0, right: 0, left: 0, bottom: 0, flex: 1, position: 'absolute' }, children));
 }
 //# sourceMappingURL=StackScreenWebContainer.js.map
--- packages/fractal-navigation-router/src/navigation/StackNavigation/components/StackScreenWebContainers/StackScreenWebContainer.tsx
+++ packages/fractal-navigation-router/src/navigation/StackNavigation/components/StackScreenWebContainers/StackScreenWebContainer.tsx
@@ -1,9 +1,14 @@
 import React, { ReactNode } from 'react';
+import { Background } from '@bma98/fractal-ui';
 
 export interface StackScreenWebContainerProps {
     children?: ReactNode;
 }
 
 export function StackScreenWebContainer({ children }: StackScreenWebContainerProps): JSX.Element {
-    return <>{children}</>;
+    return (
+        <Background top={0} right={0} left={0} bottom={0} flex={1} position={'absolute'}>
+            {children}
+        </Background>
+    );
 }
