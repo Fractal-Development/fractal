6aaf9af340c8a75adfa697815a52ea2e50d9473b
brandonmaldonadoalonso <brandonma98@protonmail.com>
2021-08-09 06:38:22 -0500

    added testing example for navigation before adding it to the main example

--- examples/fractal-ui-expo-example/App.tsx
+++ examples/fractal-ui-expo-example/App.tsx
@@ -1,64 +1,110 @@
-import React, { ReactElement, useState } from 'react';
-import { Screen, ScreenStack } from 'react-native-screens';
+import React, { ReactNode, useState } from 'react';
 import { MainExample } from '@bma98/fractal-examples';
 import {
-    NativeNavigationBar,
     NativeNavigationBarRightView,
     NavigationBarButton,
     TabBar,
     TabBarContextProvider,
     NavigationLayer,
     SimpleTabBarItem,
-    SimpleCircularTabBarItem
+    SimpleCircularTabBarItem,
+    useToggleTabBarHidden
 } from '@bma98/fractal-navigation';
-import { Layer, BugIcon, AppleIcon, SearchIcon, SafeAreaLayer } from '@bma98/fractal-ui';
+import { BugIcon, AppleIcon, SearchIcon, Button, ScrollView, Text, Layer, PaddingLayer } from '@bma98/fractal-ui';
+import { StackNavigator, StackScreen, NavigationBarConfig, NavigationRouter, useHistory } from '@bma98/fractal-navigation-router';
 
 interface NavigationProps {
-    children: ReactElement;
+    children: ReactNode;
 }
 
-export function Navigation({ children }: NavigationProps) {
-    const [currentTab, setCurrentTab] = useState('/bug');
+function TabBarIsHiddenToggle() {
+    const toggleTabBarIsHidden = useToggleTabBarHidden();
+    return <Button text={'Toggle Tab Bar'} onPress={toggleTabBarIsHidden} />;
+}
 
+function GoToSecondStack() {
+    const history = useHistory();
     return (
-        <Layer flex={1}>
-            <TabBarContextProvider>
-                <ScreenStack style={{ flex: 1 }}>
-                    <Screen>
-                        <NativeNavigationBar title={'Examples'} goBack={() => {}}>
+        <Button
+            flexShrink={0}
+            text={'Go To Second Stack'}
+            onPress={() => {
+                history.push('/second_stack');
+            }}
+        />
+    );
+}
+
+function MainTabBar() {
+    const [currentTab, setCurrentTab] = useState('/bug');
+    return (
+        <TabBar tabBarPosition={'bottom'}>
+            <SimpleTabBarItem active={currentTab === '/bug'} title={'Bug'} tabIdentifier='/bug' onTabPress={setCurrentTab}>
+                {(color, size) => {
+                    return <BugIcon fill={color} height={size} width={size} />;
+                }}
+            </SimpleTabBarItem>
+            <SimpleCircularTabBarItem tabIdentifier='/search' onTabPress={setCurrentTab}>
+                {(color, size) => {
+                    return <SearchIcon fill={color} height={size} width={size} />;
+                }}
+            </SimpleCircularTabBarItem>
+            <SimpleTabBarItem active={currentTab === '/apple'} title={'Apple'} tabIdentifier='/apple' onTabPress={setCurrentTab}>
+                {(color, size) => {
+                    return <AppleIcon fill={color} height={size} width={size} />;
+                }}
+            </SimpleTabBarItem>
+        </TabBar>
+    );
+}
+
+export function TabBarNavigation({ children }: NavigationProps) {
+    return (
+        <TabBarContextProvider>
+            {children}
+            <MainTabBar />
+        </TabBarContextProvider>
+    );
+}
+
+export function Navigation() {
+    return (
+        <TabBarNavigation>
+            <StackNavigator path={'/'}>
+                <StackScreen
+                    isRootRoute
+                    path={'/'}
+                    navBarConfig={
+                        <NavigationBarConfig title={'Main Stack'}>
                             <NativeNavigationBarRightView>
                                 <NavigationBarButton>Prueba</NavigationBarButton>
                             </NativeNavigationBarRightView>
-                        </NativeNavigationBar>
-                        <NavigationLayer>{children}</NavigationLayer>
-                    </Screen>
-                </ScreenStack>
-                <TabBar tabBarPosition={'bottom'}>
-                    <SimpleTabBarItem active={currentTab === '/bug'} title={'Bug'} tabIdentifier='/bug' onTabPress={setCurrentTab}>
-                        {(color, size) => {
-                            return <BugIcon fill={color} height={size} width={size} />;
-                        }}
-                    </SimpleTabBarItem>
-                    <SimpleCircularTabBarItem tabIdentifier='/search' onTabPress={setCurrentTab}>
-                        {(color, size) => {
-                            return <SearchIcon fill={color} height={size} width={size} />;
-                        }}
-                    </SimpleCircularTabBarItem>
-                    <SimpleTabBarItem active={currentTab === '/apple'} title={'Apple'} tabIdentifier='/apple' onTabPress={setCurrentTab}>
-                        {(color, size) => {
-                            return <AppleIcon fill={color} height={size} width={size} />;
-                        }}
-                    </SimpleTabBarItem>
-                </TabBar>
-            </TabBarContextProvider>
-        </Layer>
+                        </NavigationBarConfig>
+                    }
+                >
+                    <NavigationLayer>
+                        <ScrollView>
+                            <PaddingLayer>
+                                <GoToSecondStack />
+                            </PaddingLayer>
+                            <MainExample />
+                        </ScrollView>
+                    </NavigationLayer>
+                </StackScreen>
+                <StackScreen path={'/second_stack'} navBarConfig={<NavigationBarConfig title={'Second Stack'} />}>
+                    <NavigationLayer>
+                        <Text>Hola</Text>
+                    </NavigationLayer>
+                </StackScreen>
+            </StackNavigator>
+        </TabBarNavigation>
     );
 }
 
 export default function App() {
     return (
-        <Navigation>
-            <MainExample />
-        </Navigation>
+        <NavigationRouter style={{ flex: 1, backgroundColor: 'red' }}>
+            <Navigation />
+        </NavigationRouter>
     );
 }
