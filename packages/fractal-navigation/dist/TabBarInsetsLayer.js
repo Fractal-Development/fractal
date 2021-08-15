dd9db016c66d12da14c28d683c7788536cb04b7d
brandonmaldonadoalonso <brandonma98@protonmail.com>
2021-08-15 17:56:52 -0500

    bug fixes

--- packages/fractal-navigation/dist/TabBarInsetsLayer.js
+++ packages/fractal-navigation/dist/TabBarInsetsLayer.js
@@ -3,11 +3,15 @@ import { useTabBarInsets } from './TabBar';
 import { SafeAreaLayer } from '@bma98/fractal-ui';
 export function TabBarInsetsLayer(props) {
     const tabBarInsets = useTabBarInsets();
-    return (React.createElement(SafeAreaLayer, Object.assign({ marginTop: 0, marginRight: 0, marginLeft: 0, marginBottom: 0, from: false, animate: {
-            marginTop: tabBarInsets.top,
-            marginRight: tabBarInsets.right,
-            marginLeft: tabBarInsets.left,
-            marginBottom: tabBarInsets.bottom
-        }, transition: { type: 'timing' }, flex: 1, position: 'relative', overflow: 'hidden' }, props)));
+    return (React.createElement(SafeAreaLayer, Object.assign({ marginTop: tabBarInsets.top, marginRight: tabBarInsets.right, marginLeft: tabBarInsets.left, marginBottom: tabBarInsets.bottom, 
+        // from={false}
+        // animate={{
+        //     marginTop: tabBarInsets.top,
+        //     marginRight: tabBarInsets.right,
+        //     marginLeft: tabBarInsets.left,
+        //     marginBottom: tabBarInsets.bottom
+        // }}
+        // transition={{ type: 'timing' }}
+        flex: 1, position: 'relative', overflow: 'hidden' }, props)));
 }
 //# sourceMappingURL=TabBarInsetsLayer.js.map
--- packages/fractal-navigation/src/TabBarInsetsLayer.tsx
+++ packages/fractal-navigation/src/TabBarInsetsLayer.tsx
@@ -7,18 +7,18 @@ export function TabBarInsetsLayer(props: LayerProps): ReactElement {
 
     return (
         <SafeAreaLayer
-            marginTop={0}
-            marginRight={0}
-            marginLeft={0}
-            marginBottom={0}
-            from={false}
-            animate={{
-                marginTop: tabBarInsets.top,
-                marginRight: tabBarInsets.right,
-                marginLeft: tabBarInsets.left,
-                marginBottom: tabBarInsets.bottom
-            }}
-            transition={{ type: 'timing' }}
+            marginTop={tabBarInsets.top}
+            marginRight={tabBarInsets.right}
+            marginLeft={tabBarInsets.left}
+            marginBottom={tabBarInsets.bottom}
+            // from={false}
+            // animate={{
+            //     marginTop: tabBarInsets.top,
+            //     marginRight: tabBarInsets.right,
+            //     marginLeft: tabBarInsets.left,
+            //     marginBottom: tabBarInsets.bottom
+            // }}
+            // transition={{ type: 'timing' }}
             flex={1}
             position='relative'
             overflow={'hidden'}
