16e6f0a5696cccd28a2b46eb3c507377a23fec03
brandonmaldonadoalonso <brandonma98@protonmail.com>
2021-10-15 10:34:11 -0500

    smoother animations for native

--- packages/fractal-messaging/dist/components/ChatLoadingIndicator/config/index.native.js
+++ packages/fractal-messaging/dist/components/ChatLoadingIndicator/config/index.native.js
@@ -1,14 +1,17 @@
 export const loadingCircleVariants = {
     start: {
+        opacity: 0,
         scale: 0
     },
     end: {
+        opacity: 1,
         scale: 1
     }
 };
 export const loadingCircleTransition = {
     loop: true,
-    duration: 600
+    duration: 600,
+    type: 'timing'
 };
 export const loadingCircleTransitionTwo = Object.assign(Object.assign({}, loadingCircleTransition), { delay: 300 });
 export const loadingCircleTransitionThree = Object.assign(Object.assign({}, loadingCircleTransition), { delay: 600 });
