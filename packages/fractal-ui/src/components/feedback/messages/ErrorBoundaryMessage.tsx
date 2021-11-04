f7aee57efb1addd9a1d47972de56023ee25c10c6
bguerreroGit <69155714+baltazar-gr@users.noreply.github.com>
2021-11-04 13:09:28 -0600

    fractal-ui: create ErrorBoundaryMessage

---
+++ packages/fractal-ui/src/components/feedback/messages/ErrorBoundaryMessage.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Layer, LayerProps } from '../../layout';
+import { ErrorMessage } from './ErrorMessage';
+
+interface State {
+    hasError: boolean;
+    errorMessage: string;
+    errorTitle: string;
+}
+
+interface ErrorMessageProps extends LayerProps {
+    onError?: (error: Error, componentStack: string) => void;
+}
+
+export class ErrorBoundaryMessage extends React.Component<ErrorMessageProps, State> {
+    constructor(props: ErrorMessageProps) {
+        super(props);
+        this.state = { hasError: false, errorMessage: '', errorTitle: '' };
+    }
+
+    static getDerivedStateFromError(error: Error): State {
+        return { hasError: true, errorMessage: error.message, errorTitle: error.name };
+    }
+
+    componentDidCatch(error: Error, info: React.ErrorInfo): void {
+        const { onError } = this.props;
+        if (onError) onError(error, info.componentStack);
+    }
+
+    render(): React.ReactNode {
+        const { hasError, errorTitle, errorMessage } = this.state;
+        const { onError, ...layerProps } = this.props;
+
+        if (hasError) {
+            return <ErrorMessage title={errorTitle} description={errorMessage} {...layerProps} />;
+        }
+
+        return <Layer {...layerProps} />;
+    }
+}
--- packages/fractal-ui/src/components/feedback/messages/ErrorMessage.tsx
+++ packages/fractal-ui/src/components/feedback/messages/ErrorMessage.tsx
@@ -1,44 +1,11 @@
 import React from 'react';
-import { Message } from './Message';
-import { BugIcon } from '../../../assets';
-import { Layer, LayerProps } from '../../layout';
+import { Message, MessageProps } from './Message';
+import { BugIcon } from '../../../assets/BugIcon';
 
-interface State {
-    hasError: boolean;
-    errorMessage: string;
-    errorTitle: string;
-}
+type ErrorMessageProps = Omit<MessageProps, 'messageType' | 'icon'>;
 
-interface ErrorMessageProps extends LayerProps {
-    onError?: (error: Error, componentStack: string) => void;
-}
+export function ErrorMessage(props: ErrorMessageProps): JSX.Element {
+    const renderErrorIcon = (color: string): JSX.Element => <BugIcon width={20} fill={color} />;
 
-export class ErrorMessage extends React.Component<ErrorMessageProps, State> {
-    constructor(props: ErrorMessageProps) {
-        super(props);
-        this.state = { hasError: false, errorMessage: '', errorTitle: '' };
-    }
-
-    static getDerivedStateFromError(error: Error): State {
-        return { hasError: true, errorMessage: error.message, errorTitle: error.name };
-    }
-
-    componentDidCatch(error: Error, info: React.ErrorInfo): void {
-        const { onError } = this.props;
-        if (onError) onError(error, info.componentStack);
-    }
-
-    renderErrorIcon = (color: string): JSX.Element => <BugIcon width={20} fill={color} />;
-
-    render(): React.ReactNode {
-        const { hasError, errorTitle, errorMessage } = this.state;
-
-        if (hasError) {
-            return (
-                <Message messageType='danger' title={errorTitle} description={errorMessage} icon={this.renderErrorIcon} {...this.props} />
-            );
-        }
-
-        return <Layer {...this.props} />;
-    }
+    return <Message messageType='danger' icon={renderErrorIcon} {...props} />;
 }
