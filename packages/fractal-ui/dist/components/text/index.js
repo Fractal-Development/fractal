35b40fbf3b2e4f43ed7df4fc0434c5ef7653fd52
brandonmaldonadoalonso <brandonma98@protonmail.com>
2021-12-14 12:40:12 -0600

    new build

--- packages/fractal-ui/dist/components/text/index.js
+++ packages/fractal-ui/dist/components/text/index.js
@@ -2,4 +2,5 @@ export * from './BaseText';
 export * from './BaseText/types';
 export * from './Text';
 export * from './RichText';
+export * from './RichText/types';
 //# sourceMappingURL=index.js.map
