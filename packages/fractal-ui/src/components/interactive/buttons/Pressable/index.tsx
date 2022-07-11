fe74dc366e926bf39ce2916efb5e65b69f60ec84
RVFractal <rubenvillegas@fractal.software>
2022-07-11 09:29:47 -0500

    fractal-ui: update Pressable component

--- packages/fractal-ui/src/components/interactive/buttons/Pressable/index.tsx
+++ packages/fractal-ui/src/components/interactive/buttons/Pressable/index.tsx
@@ -30,7 +30,7 @@ const Pressable = forwardRef(
         <StyledPressable
             ref={ref}
             flexDirection='column'
-            initial={currentVariant ?? animate}
+            initial={currentVariant || from}
             animate={currentVariant ?? animate}
             transition={transition}
             onClick={onPress}
