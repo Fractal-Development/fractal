7061cf291225dd07cc26279888d47ff57436385b
Brandon Maldonado Alonso <brandonma98@protonmail.com>
2022-07-11 12:15:37 -0500

    Fix react js project

--- apps/fractal-ui-react-js-example/src/App.tsx
+++ apps/fractal-ui-react-js-example/src/App.tsx
@@ -1,13 +1,7 @@
 import React from 'react';
-// import { MainExample } from '@bma98/fractal-examples';
+import { MainExample } from '@bma98/fractal-examples';
 import { FractalAppRoot, SafeAreaLayer, Text } from '@bma98/fractal-ui';
-console.log(FractalAppRoot);
+
 export default function App() {
-    return (
-        <FractalAppRoot>
-            <SafeAreaLayer>
-                <Text>Hola</Text>
-            </SafeAreaLayer>
-        </FractalAppRoot>
-    );
+    return <MainExample />;
 }
--- packages/fractal-media/src/components/Video/index.native.tsx
+++ packages/fractal-media/src/components/Video/index.native.tsx
@@ -1,12 +1,10 @@
 import React, { forwardRef } from 'react';
-import { Video as ExpoVideo, VideoProps as ExpoVideoProps } from 'expo-av';
+import { Video as ExpoVideo } from 'expo-av';
 import { VideoProps } from './types';
-
-const ExpoVideoComponent = ExpoVideo as unknown as React.FC<ExpoVideoProps>;
 
 const Video = forwardRef(
     ({ source, ...others }: VideoProps, ref: any): JSX.Element => (
-        <ExpoVideoComponent ref={ref} source={typeof source === 'string' ? { uri: source } : source} {...others} />
+        <ExpoVideo ref={ref} source={typeof source === 'string' ? { uri: source } : source} {...others} />
     )
 );
 
