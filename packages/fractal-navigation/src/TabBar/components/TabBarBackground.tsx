0926afdc3477e5b24d29e242e78af5776a7869a7
RVFractal <rubenvillegas@fractal.software>
2022-07-19 10:59:43 -0500

    update TabBarBackground backgroundColor condition

--- packages/fractal-navigation/src/TabBar/components/TabBarBackground.tsx
+++ packages/fractal-navigation/src/TabBar/components/TabBarBackground.tsx
@@ -5,19 +5,18 @@ import { useTabBarPositionLayoutProps, useTabBarSafeAreaPadding } from '../hooks
 import { tabBarCSSPosition } from '../util/tabBarCSSPosition';
 
 export function TabBarBackground(props: TabBarProps): JSX.Element {
-    const { tabBarPosition } = props;
+    const { tabBarPosition, backgroundColor } = props;
     const { tabBar } = useTheme();
     const { translateY, translateX, ...layoutProps } = useTabBarPositionLayoutProps(tabBarPosition);
     const tabBarSafeAreaPadding = useTabBarSafeAreaPadding(tabBarPosition);
     const justifyContent = tabBarPosition === 'bottom' ? 'center' : 'flex-start';
     const shadow =
         tabBarPosition === 'bottom' ? tabBar.shadow : tabBarPosition === 'left' ? tabBar.leftPositionShadow : tabBar.rightPositionShadow;
-
     return (
         <Layer
             animate={{ translateY, translateX }}
             transition={{ type: 'timing' }}
-            backgroundColor={tabBar.backgroundColor}
+            backgroundColor={backgroundColor || tabBar.backgroundColor}
             boxShadow={shadow}
             justifyContent={justifyContent}
             position={tabBarCSSPosition}
