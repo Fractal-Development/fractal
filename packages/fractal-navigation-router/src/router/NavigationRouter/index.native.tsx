986fd9b30eb5679c58812f9ddfc2aa84b3507e21
baltazar-gr <69155714+baltazar-gr@users.noreply.github.com>
2022-09-27 11:51:06 -0500

    fix children prop to react-18

--- packages/fractal-navigation-router/src/router/NavigationRouter/index.native.tsx
+++ packages/fractal-navigation-router/src/router/NavigationRouter/index.native.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { BackButton, NativeRouter } from 'react-router-native';
 
-export const NavigationRouter: React.FC = ({ children }) => (
+export const NavigationRouter: React.FC<{
+    children?: React.ReactNode;
+}> = ({ children }) => (
     <NativeRouter>
         <BackButton>{children}</BackButton>
     </NativeRouter>
--- packages/fractal-navigation-router/src/router/NavigationRouter/index.tsx
+++ packages/fractal-navigation-router/src/router/NavigationRouter/index.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
-export const NavigationRouter: React.FC = ({ children }) => <BrowserRouter>{children}</BrowserRouter>;
+export const NavigationRouter: React.FC<{
+    children?: React.ReactNode;
+}> = ({ children }) => <BrowserRouter>{children}</BrowserRouter>;
