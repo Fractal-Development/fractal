27fb195a9bd66c7ea3f1f2d263397b6b14e333f0
baltazar-gr <69155714+baltazar-gr@users.noreply.github.com>
2022-11-02 13:12:16 -0600

    fix BaseTextField

--- packages/fractal-ui/src/components/interactive/textInputs/BaseTextField/index.tsx
+++ packages/fractal-ui/src/components/interactive/textInputs/BaseTextField/index.tsx
@@ -9,12 +9,15 @@ import {
     extractDisplayProps,
     extractTextProps,
     extractWebProps,
-    extractPlaceholder
+    extractPlaceholder,
+    shouldForwardProp
 } from '../../../../sharedProps';
 import { getBaseTextFieldAccessibilityProps } from '../accessibility/getBaseTextFieldAccessibilityProps';
 import { TextFieldProps } from './types';
 
-const StyledTextInput = styled(motion.input as any)`
+const StyledTextInput = styled(motion.input as any).withConfig({
+    shouldForwardProp
+})`
     outline: none;
     border-width: 0;
     ${extractPlaceholder};
@@ -24,11 +27,22 @@ const StyledTextInput = styled(motion.input as any)`
     ${extractBorderProps};
     ${extractTextProps};
     ${extractWebProps};
+    user-select: auto;
 `;
 
 const BaseTextField = forwardRef(
     (
-        { onChangeText, onSubmitEditing, placeholder, secureTextEntry, from, currentVariant, animate, ...others }: TextFieldProps,
+        {
+            onChangeText,
+            onSubmitEditing,
+            placeholder,
+            secureTextEntry,
+            from,
+            currentVariant,
+            animate,
+            placeholderTextColor,
+            ...others
+        }: TextFieldProps,
         ref: Ref<HTMLInputElement>
     ): JSX.Element => {
         const handleChange = (event: { target: { value: string } }): void => onChangeText && onChangeText(event.target.value);
@@ -49,6 +63,7 @@ const BaseTextField = forwardRef(
                 type={secureTextEntry ? 'password' : undefined}
                 initial={currentVariant ? 'from' : from}
                 animate={currentVariant ?? animate}
+                placeholderTextColor={placeholderTextColor}
                 {...getBaseTextFieldAccessibilityProps(placeholder)}
                 {...others}
             />
